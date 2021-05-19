d11ce693101e2071e05bc60258b0be4f17eb349c
Harjas Singh Anand <harjasrock@gmail.com>
2021-05-19 21:20:41 +0530

    Removed defaults

---
+++ frontend/src/scenes/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Layout } from 'antd'
+
+const App = (): JSX.Element | null => {
+    return (
+        <>
+            <Layout style={{ minHeight: '100vh' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At repudiandae rerum exercitationem
+                consectetur, sequi temporibus ducimus nemo alias esse atque. Expedita, esse magnam autem quos pariatur
+                voluptatem exercitationem veritatis doloribus?
+            </Layout>
+        </>
+    )
+}
+
+export default App
