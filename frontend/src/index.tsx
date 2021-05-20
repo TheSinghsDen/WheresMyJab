e56d2c86c34af37f24a81919c52c2576ece185b1
Harjas Singh Anand <harjasrock@gmail.com>
2021-05-20 12:41:42 +0530

    Added ant.d styles to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './scenes/App'
+
+import { GlobalStyles } from './GlobalStyles'
 
 ReactDOM.render(
     <div>
+        <GlobalStyles />
         <App />
     </div>,
 
