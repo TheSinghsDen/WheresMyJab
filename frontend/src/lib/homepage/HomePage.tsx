56dbcd0a3542eed5f80ec27510f7731c3b1ba68a
Harjas Singh Anand <harjasrock@gmail.com>
2021-05-20 14:36:48 +0530

    Added title and select to homepage

--- frontend/src/lib/homepage/HomePage.tsx
+++ frontend/src/lib/homepage/HomePage.tsx
@@ -1,12 +1,46 @@
 import React from 'react'
-import { Row, Col } from 'antd'
+import { Row, Typography, Select } from 'antd'
+import './index.scss'
+
+const { Title } = Typography
+const { Option } = Select
 
 const HomePage = (): JSX.Element => {
     return (
-        <Row justify="center" align="middle">
-            <Col span={4}>col</Col>
-            <Col span={4}>col</Col>
-        </Row>
+        <div>
+            <Row justify="center" align="middle" className="title">
+                <Title level={2}>WheresMyJab</Title>
+            </Row>
+            <Row justify="center" align="middle" className="select">
+                <Select
+                    showSearch
+                    style={{ width: '100%' }}
+                    size="large"
+                    placeholder="State"
+                    optionFilterProp="children"
+                    filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                >
+                    <Option value="jack">Jack</Option>
+                    <Option value="lucy">Lucy</Option>
+                    <Option value="tom">Tom</Option>
+                </Select>
+            </Row>
+
+            <Row justify="center" align="middle" className="select">
+                <Select
+                    showSearch
+                    style={{ width: '100%' }}
+                    size="large"
+                    placeholder="District"
+                    optionFilterProp="children"
+                    filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                >
+                    <Option value="jack">Jack</Option>
+                    <Option value="lucy">Lucy</Option>
+                    <Option value="tom">Tom</Option>
+                </Select>
+            </Row>
+        </div>
     )
 }
 
