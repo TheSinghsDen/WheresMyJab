dde51f3f60e35d3335ee2336337a9ccdf530d40e
Harjas Singh Anand <harjasrock@gmail.com>
2021-06-13 23:06:58 +0530

    Deploying to heroku,3

--- wheresmyjab/apps.py
+++ wheresmyjab/apps.py
@@ -5,6 +5,6 @@ class WheresMyJabConfig(AppConfig):
     name = "wheresmyjab"
     verbose_name = "WheresMyJab"
 
-    def ready(self):
-        from wheresmyjab.tasks.poll_cowin import poll_cowin_for_updates
-        poll_cowin_for_updates()
+    # def ready(self):
+    #     from wheresmyjab.tasks.poll_cowin import poll_cowin_for_updates
+    #     poll_cowin_for_updates()
